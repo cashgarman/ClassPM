9e46131b8cce94ca77a37a2681fbddf689e1f18a
Cash Garman <cashgarman@hotmail.co.uk>
2020-08-20 17:27:43 -0700

    Added lesson 10 content.

---
+++ Assets/VRHand.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using UnityEngine;
+
+public class VRHand : MonoBehaviour
+{
+    public string triggerName;
+
+    private MeshRenderer handMesh;
+    private bool triggerHeld;
+    private Color originalColor;
+    private GameObject selectedObject;
+    private Material selectedObjectMaterial;
+    private Color selectedObjectOriginalColor;
+
+    private void Start()
+    {
+        handMesh = GetComponent<MeshRenderer>();
+        originalColor = handMesh.material.color;
+    }
+
+    void Update()
+    {
+	    if(Input.GetAxis(triggerName) > 0.8f && !triggerHeld)
+	    {
+			triggerHeld = true;
+			handMesh.material.color = Color.green;
+	    }
+	    else if(Input.GetAxis(triggerName) < 0.8f && triggerHeld)
+	    {
+		    triggerHeld = false;
+		    handMesh.material.color = originalColor;
+	    }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+	    Debug.Log($"{name} entered the collider of {other.name}");
+	    selectedObjectMaterial = other.GetComponent<MeshRenderer>().material;
+	    selectedObjectOriginalColor = selectedObjectMaterial.color;
+	    selectedObjectMaterial.color = Color.red;
+	    selectedObject = other.gameObject;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+	    if (selectedObject != null)
+	    {
+		    other.GetComponent<MeshRenderer>().material.color = selectedObjectOriginalColor;
+	    }
+    }
+}
---
+++ Assets/VRMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRMovement : MonoBehaviour
+{
+    public string forwardInput;
+
+    void Update()
+    {
+        if (Input.GetAxis(forwardInput) != 0)
+        {
+            // Move the VRRoot forward in the direction the VRHead is facing
+            transform.parent.Translate(transform.forward * -Input.GetAxis(forwardInput) * Time.deltaTime);
+        }
+    }
+}
