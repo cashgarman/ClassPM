958cb0609b6d6a39180b71ac3acd87f7160d2925
Cash Garman <cashgarman@hotmail.co.uk>
2020-08-28 10:19:44 -0700

    Lesson 12 push.

---
+++ Assets/Clock.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+	public MinuteHand minuteHand;
+
+	private void Update()
+	{
+		if (Input.GetKey(KeyCode.Space))
+		{
+			Time.timeScale = 20f;
+		}
+		else
+		{
+			Time.timeScale = 1f;
+		}
+	}
+
+	public void OnMinutePassed()
+	{
+		minuteHand.OnAdvance();
+	}
+}
---
+++ Assets/SecondHand.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class SecondHand : MonoBehaviour
+{
+    public Clock clock;
+
+    private void Start()
+    {
+        GetComponent<Animator>().Play("Secondhand", 0, Random.Range(0f, 1f));
+    }
+
+    public void OnMinutePassed()
+    {
+        clock.OnMinutePassed();
+    }
+}
