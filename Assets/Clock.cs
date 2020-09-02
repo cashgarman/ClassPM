3956a17d944a73fbd7d4004af4009abbff49a14e
Cash Garman <cashgarman@hotmail.co.uk>
2020-09-01 17:30:41 -0700

    Added assets for VR hands.

---
+++ Assets/Clock.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+    public Transform minuteHand;
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Time.timeScale = 20f;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+        }
+    }
+
+    internal void OnMinutePassed()
+    {
+        minuteHand.Rotate(0, 0, -360f / 60f);
+    }
+}
