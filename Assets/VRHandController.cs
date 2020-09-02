aaa0a9b7dc903e3d0273b8667c6bffe1d9332cc1
Cash Garman <cashgarman@hotmail.co.uk>
2020-09-01 20:44:15 -0700

    Lesson 13 progress.

---
+++ Assets/VRHandController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRHandController : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger("Grip");
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            animator.SetTrigger("Ungrip");
+        }
+    }
+}
