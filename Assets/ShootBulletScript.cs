365727032f58b47e5ea414697d4efd07482bd5cf
seanstrocity <30853911+seanstrocity@users.noreply.github.com>
2020-09-08 21:02:57 -0700

    sean's branch

---
+++ Assets/ShootBulletScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootBulletScript : MonoBehaviour
+{
+    public Transform bulletSpawnPoint;
+    public GameObject bulletPrefab;
+    public float shootForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+           GameObject bullet = Instantiate(bulletPrefab, bulletSpawnPoint);
+            bullet.GetComponent<Rigidbody>().AddForce(transform.forward * shootForce);
+        }
+    }
+}
---
+++ Assets/VRHandController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRHandController : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Start()
+    {
+
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger("Grip");
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            animator.SetTrigger("Ungrip");
+        }
+    }
+}
